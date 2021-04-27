98c741168ac6201167d61528384033c7d327cb2b
desawarna <desawarna@gmail.com>
2021-04-27 20:33:01 +0700

    Template Jurnal Logo

---
+++ parts/_logo_jurnal.php
@@ -0,0 +1,130 @@
+<!-- Logo Jurmal DSWRNA  -->
+<?php if ($sysconf['template']['classic_logo_jurnal']) : ?>
+<div class='section-wrapper logo-jurnal'>
+		<div class='container'>
+			<h4>E-Resources</h4>
+			<div class='owl-carousel logo-jurnal-carousel'>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img0.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img1.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img2.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img3.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img4.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img5.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img6.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img7.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img8.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img9.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img10.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img11.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img12.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img13.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img14.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img15.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img16.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img17.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+				<div class='item'>
+					<a href='#'>
+						<img src='<?php echo assets('images/logo-jurnal/img18.png'); ?>' alt='logo-jurnal-brand' class='img-responsive logo-jurnal-item'>
+					</a>
+				</div>
+			</div> <!-- /.owl-carousel -->
+		</div> <!-- /.container -->
+	</div> <!-- /.logo-jurnal -->
+	<!-- Logo Jurmal DSWRNA  -->
+    <script>
+        $(document).ready(function() {
+    $('.logo-jurnal-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		autoplay:true,
+		autoplayTimeout:1000,
+		autoplayHoverPause:true,
+		nav: false,
+		navText: ['', ''],
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+	});
+    </script>
+<?php endif; ?>
